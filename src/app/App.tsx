2e1062eee81db1082c7edd4aa1badc735c6be718
swson <xhxh9539@gmail.com>
2026-05-07 00:22:20 +0900

    Revert "카드 개선 2/5: Featured/Compact 분리 + impact chip + tech tag + hover 상승"
    This reverts commit 7f81c8a93b57e280c4d9dc425c5a8226f03993ea.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -102,7 +102,7 @@ export default function App() {
               .filter((p) => FEATURED_IDS.includes(p.id))
               .sort((a, b) => FEATURED_IDS.indexOf(a.id) - FEATURED_IDS.indexOf(b.id))
               .map((item, index) => (
-                <PortfolioCard key={item.id} item={item} index={index} featured />
+                <PortfolioCard key={item.id} item={item} index={index} />
               ))}
           </div>
 
