5b8ed4cb02fb73ef18b55dae51313920eded8fc1
swson <xhxh9539@gmail.com>
2026-05-06 23:28:36 +0900

    Revert "상세 페이지 2차 UI: 공통 컴포넌트 + Callbot 메인 톤 통일"
    This reverts commit 32c6324bf1a988ffb711772f8e836cd8ec98107c.

--- src/app/components/detail/index.ts
+++
@@ -1,6 +0,0 @@
-export { DetailSection } from "./DetailSection";
-export { DetailMetricCard } from "./DetailMetricCard";
-export { DetailStep } from "./DetailStep";
-export { DetailBulletItem } from "./DetailBulletItem";
-export { DetailGuardrailItem } from "./DetailGuardrailItem";
-export { DetailImage } from "./DetailImage";
