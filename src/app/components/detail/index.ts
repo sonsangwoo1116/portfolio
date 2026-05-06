f8fedb79df7022ec02dd12d3ef7275e8f1a5ce22
swson <xhxh9539@gmail.com>
2026-05-06 23:28:37 +0900

    Revert "상세 페이지 2차 UI 개선: 공통 컴포넌트 + Callbot 적용"
    This reverts commit 36951bc24fab2ae4557d0f37653e2456ca700eab.

--- src/app/components/detail/index.ts
+++
@@ -1,7 +0,0 @@
-export { DetailHero } from "./DetailHero";
-export { DetailSection } from "./DetailSection";
-export { DetailMetricCard } from "./DetailMetricCard";
-export { DetailStep } from "./DetailStep";
-export { DetailBulletItem } from "./DetailBulletItem";
-export { DetailGuardrailItem } from "./DetailGuardrailItem";
-export { DemoImage } from "./DemoImage";
